99af65c799796240fb62b9e230d1c41bd6b5063b
Ramniwas Kumawat <ramniwas8824@gmail.com>
2022-12-20 01:33:28 +0530

    new changes done

--- app/src/main/java/com/dhanuka/safeobuddy/activity/SplashActivity.java
+++ app/src/main/java/com/dhanuka/safeobuddy/activity/SplashActivity.java
@@ -1,8 +1,14 @@
 package com.dhanuka.safeobuddy.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codersworld.rklib.scanner.ScanActivity;
 import com.codersworld.rklib.utils.AppUrls;
 import com.codersworld.rklib.utils.CommonMethods;
 import com.dhanuka.safeobuddy.R;
@@ -14,6 +20,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        CommonMethods.successToast(this, AppUrls.GET_UOM_ITEM);
+        startActivityForResult(new Intent(this, ScanActivity.class).putExtra("open_scanner", true), 102);
+        // CommonMethods.successToast(this, AppUrls.GET_UOM_ITEM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 102) {
+            try {
+                Log.e("MRESULT",data.getStringExtra("result"));
+                Toast.makeText(this, data.getStringExtra("result") + "", Toast.LENGTH_SHORT).show();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
---
+++ safelib/src/main/java/com/codersworld/rklib/scanner/ScanActivity.kt
@@ -0,0 +1,68 @@
+package com.codersworld.rklib.scanner
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.Toast
+import com.codersworld.rklib.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import io.github.g00fy2.quickie.ScanCustomCode
+import io.github.g00fy2.quickie.ScanQRCode
+import io.github.g00fy2.quickie.config.BarcodeFormat
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.config.ScannerConfig
+
+import io.github.g00fy2.quickie.QRResult.QRError
+import io.github.g00fy2.quickie.QRResult.QRMissingPermission
+import io.github.g00fy2.quickie.QRResult.QRSuccess
+import io.github.g00fy2.quickie.QRResult.QRUserCanceled
+
+class ScanActivity : AppCompatActivity() {
+
+
+    private var selectedBarcodeFormat = BarcodeFormat.FORMAT_ALL_FORMATS
+    lateinit var qr_scanner_button: Button
+    private val scanQrCode = registerForActivityResult(ScanQRCode(), ::showSnackbar)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_scan)
+        // qr_scanner_button = findViewById(R.id.qr_scanner_button)
+        /* qr_scanner_button.setOnClickListener {
+             scanQrCode.launch(null)
+         }*/
+        if (intent.extras?.getBoolean("open_scanner") == true) scanQrCode.launch(null)
+    }
+
+    private fun showSnackbar(result: QRResult) {
+        try {
+            Log.e("resultresult", Gson().toJson(result))
+        } catch (ex: java.lang.Exception) {
+            ex.printStackTrace()
+            Log.e("resultqwe", result.toString())
+        }
+        val text = when (result) {
+            is QRSuccess -> result.content.rawValue
+            QRUserCanceled -> "User canceled"
+            QRMissingPermission -> "Missing permission"
+            is QRError -> "${result.exception.javaClass.simpleName}: ${result.exception.localizedMessage}"
+        }
+        Log.e("mIntent", text.toString())
+        var mIntent: Intent = Intent();
+        setResult(102, mIntent.putExtra("result", text))
+        finish()
+        //Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        const val OPEN_SCANNER = "open_scanner"
+    }
+
+}
